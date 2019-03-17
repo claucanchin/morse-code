e2616cf88f5a85a9bd36614e137f06a57118ec94
claucanchin <claucanchin@gmail.com>
2019-03-17 22:42:26 +0800

    completed part1 (encoding the given strings)

--- morse.js
+++ morse.js
@@ -27,3 +27,36 @@ var alphabet = {
   y: '-.--',
   z: '--..'
 };
+
+// console.log(Object.keys(alphabet));
+
+var firstWord = "hello world"
+var secondWord = "we love javascript"
+
+//splitting the strings into array
+var firstWordArray = firstWord.split("");
+var secondWordArray = secondWord.split("");
+
+//return an array of alphabet's property names (a, b, c...)
+var alphabetArray = Object.keys(alphabet);
+//console.log(alphabetArray);
+
+//function to match alphabetArray with someArray
+var encode = function(someArray) {
+
+    var encodedArray = [];
+
+    for (i = 0; i < someArray.length; i++) {
+        for (j = 0; j < alphabetArray.length; j++) {
+            if (someArray[i] === alphabetArray[j]) {
+                encodedArray.push(alphabet[alphabetArray[j]]);
+            }
+        }
+    }
+    return encodedArray;
+}
+
+console.log("hello world");
+console.log(encode(firstWordArray));
+console.log("we love javascript")
+console.log(encode(secondWordArray));
